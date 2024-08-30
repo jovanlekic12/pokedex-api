b601169eddab91b73f159a3134db38fbb78cf9e0
jovanlekic12 <lekicjovan1212@gmail.com>
2024-08-30 22:35:05 +0200

    add displaying

--- main.js
+++ main.js
@@ -20,3 +20,52 @@ searchInput.addEventListener(
   () => (searchInputValue = searchInput.value)
 );
 const baseUrl = "https://pokeapi.co/api/v2/";
+
+const getPokemonsByType = async (pokemonType) => {
+  try {
+    const response = await fetch(baseUrl + `type/${pokemonType}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function displayPokemonsList(data) {
+  pokemonList.innerHTML = "";
+  data.forEach((pokemon) => {
+    const html = `<li class="pokemon-list-item">
+                    <p>${pokemon.pokemon.name}</p>
+                  </li>`;
+    pokemonList.insertAdjacentHTML("beforeend", html);
+  });
+}
+
+pokemonTypeList.addEventListener("click", (event) => {
+  getPokemonsByType(event.target.closest("li").getAttribute("data-type"))
+    .then((data) => {
+      displayPokemonsList(data.pokemon);
+    })
+    .catch((err) => console.log(err.message));
+});
+
+const getPokemonByName = async (pokemonName) => {
+  try {
+    const response = await fetch(baseUrl + `pokemon/${pokemonName}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function displaySelectedPokemon(image, name, height, weight, type, id) {
+  pokemonImage.src = image;
+  pokemonName.textContent = name;
+  pokemonHeight.textContent = `${height} ft`;
+  pokemonWeight.textContent = `${weight} lb`;
+  pokemonType.textContent = type;
+  pokemonId.textContent = id;
+  overlay.style.visibility = "visible";
+  selectedPokemon.style.visibility = "visible";
+}
